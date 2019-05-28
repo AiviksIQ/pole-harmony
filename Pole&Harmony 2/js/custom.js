34ca6cd18400cbe68e1022cfa39fd5f8c605aa00
AiviksIQ <37236356+AiviksIQ@users.noreply.github.com>
2019-05-28 21:25:27 +0300

    Add files via upload

---
+++ Pole&Harmony 2/js/custom.js
@@ -0,0 +1,204 @@
+// Custom Scripts for Primal Template //
+
+jQuery(function($) {
+    "use strict";
+
+
+        // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+        var mainbottom = $('#main').offset().top;
+
+        // on scroll,
+        $(window).on('scroll',function(){
+
+        // we round here to reduce a little workload
+        stop = Math.round($(window).scrollTop());
+        if (stop > mainbottom) {
+            $('.navbar').addClass('past-main');
+            $('.navbar').addClass('effect-main')
+        } else {
+            $('.navbar').removeClass('past-main');
+       }
+
+      });
+
+
+  // Collapse navbar on click
+
+   $(document).on('click.nav','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+    $(this).removeClass('in').addClass('collapse');
+   }
+  });
+
+
+
+    /*-----------------------------------
+    ----------- Scroll To Top -----------
+    ------------------------------------*/
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1000) {
+          $('#back-top').fadeIn();
+      } else {
+          $('#back-top').fadeOut();
+      }
+    });
+    // scroll body to 0px on click
+    $('#back-top').on('click', function () {
+      $('#back-top').tooltip('hide');
+      $('body,html').animate({
+          scrollTop: 0
+      }, 1500);
+      return false;
+    });
+
+
+
+
+
+  /*-------- Owl Carousel ---------- */
+    $(".reviews").owlCarousel({
+
+    slideSpeed : 200,
+    items: 1,
+    singleItem: true,
+    autoPlay : true,
+    pagination : false
+    });
+
+
+  /* ------ Clients Section Owl Carousel ----- */
+
+    $(".clients").owlCarousel({
+    slideSpeed : 200,
+    items: 5,
+    singleItem: false,
+    autoPlay : true,
+    pagination : false
+    });
+
+
+  /* ------ jQuery for Easing min -- */
+
+    $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+  });
+
+
+
+/* --------- Wow Init ------ */
+
+  new WOW().init();
+
+
+  /* ----- Counter Up ----- */
+
+$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+});
+
+
+/*----- Preloader ----- */
+
+    $(window).load(function() {
+  		setTimeout(function() {
+        $('#loading').fadeOut('slow', function() {
+        });
+      }, 3000);
+    });
+
+});
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true,
+    groupCells: true,
+    video:true,
+    autoPlay: true,
+
+});
+
+//
+// var userFeed = new Instafeed({
+//     get: 'user',
+//     userId: '13189423',
+//     accessToken: '13189423.1677ed0.fc18d8948afa4db38ec260bd77a3ecb7',
+//     limit: 12,
+//     resolution: 'standard_resolution',
+//     template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
+//
+// });
+// userFeed.run();
+
+
+
+$(function()
+{
+    function after_form_submitted(data)
+    {
+        if(data.result == 'success')
+        {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        }
+        else
+        {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors,function(key,val)
+            {
+                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function()
+            {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if(label)
+                {
+                    $btn.prop('type','submit' );
+                    $btn.text(label);
+                    $btn.prop('orig_label','');
+                }
+            });
+
+        }//else
+    }
+
+    $('#reused_form').submit(function(e)
+    {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function()
+        {
+            $btn = $(this);
+            $btn.prop('type','button' );
+            $btn.prop('orig_label',$btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+        $.ajax({
+            type: "POST",
+            url: 'handler.php',
+            data: $form.serialize(),
+            success: after_form_submitted,
+            dataType: 'json'
+        });
+
+    });
+});
